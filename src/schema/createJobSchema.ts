8029d6eb3f7d785425d0c6c8e5d3100b54081c93
algomachine007 <amaben202phantom@gmail.com>
2024-04-14 09:23:12 +0100

    completing schema creation

--- src/schema/createJobSchema.ts
+++ src/schema/createJobSchema.ts
@@ -1,9 +1,39 @@
-import { jobTypes } from "@/lib/job-type";
+import { jobTypes, locationTypes } from "@/lib/job-type";
 import { z } from "zod";
 
 const requiredString = z.string().min(1, "Required");
 const numericRequiredString = requiredString.regex(/^\d+$/, "Must be a number");
 
+const applicationSchema = z.object({
+  applicationEmail: z.string().max(100).email().optional().or(z.literal("")),
+  applicationUrl: z
+    .string()
+    .max(100)
+    .url()
+    .optional()
+    .refine((data: any) => data.applicationEmail | data.applicationUrl, {
+      message: "Email or URL is required",
+      path: ["applicationEmail"],
+    }),
+});
+const locationSchema = z.object({
+  locationType: requiredString.refine(
+    (value) => locationTypes.includes(value),
+    "Invalid location type",
+  ),
+  location: z
+    .string()
+    .max(100)
+    .optional()
+    .refine(
+      (data: any) =>
+        !data.locationType || data.locationType === "Remote" || data.location,
+      {
+        message: "Location is required for on-site jobs",
+        path: ["location"],
+      },
+    ),
+});
 export const createJobSchema = z
   .object({
     title: requiredString.max(100),
@@ -23,7 +53,7 @@ export const createJobSchema = z
         return !file || file.size < 1024 * 1024 * 2;
       }, "File must be less than 2mb"),
     description: requiredString.max(5000).optional(),
-    salary: numericRequiredString.max(9),
+    salary: numericRequiredString.max(9, "Number cant be longer than 9 digits"),
   })
   .and(applicationSchema)
   .and(locationSchema);
