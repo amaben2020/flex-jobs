8aa70d81de78ba21ad165ecf373dc488b352199a
algomachine007 <amaben202phantom@gmail.com>
2024-04-15 16:52:48 +0100

    infotitle fix

--- src/components/InfoTitle/index.tsx
+++ src/components/InfoTitle/index.tsx
@@ -1,5 +1,5 @@
 import { IHomePage } from "@/app/page";
-import { getTitle } from "../../../utils/getTitle";
+import { getTitle } from "@utils/getTitle";
 
 const InfoTitle = ({
   mainText,
@@ -8,18 +8,13 @@ const InfoTitle = ({
 }: {
   subText: string;
   mainText?: string;
-  searchParams?: IHomePage["searchParams"];
+  searchParams: IHomePage["searchParams"];
 }) => {
   return (
     <section className="mx-auto my-10 space-y-4 text-center ">
-      {!searchParams?.location ||
-      (!searchParams.location && mainText?.length) ? (
-        <h1 className="text-5xl font-bold text-gray-800">{mainText}</h1>
-      ) : (
-        <h1 className="text-5xl font-bold text-gray-800">
-          {getTitle(searchParams)}
-        </h1>
-      )}
+      <h1 className="text-5xl font-bold text-gray-800">
+        {getTitle(searchParams)}
+      </h1>
 
       <h2 className="text-xl text-gray-600">{subText}</h2>
     </section>
