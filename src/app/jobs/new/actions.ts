5ff212b064bdf2ef1275e7e8bd253c0d91165253
algomachine007 <amaben202phantom@gmail.com>
2024-04-15 09:46:01 +0100

    formatting absolute imports

--- src/app/jobs/new/actions.ts
+++ src/app/jobs/new/actions.ts
@@ -1,6 +1,9 @@
 "use server";
-
+import { nanoid } from "nanoid";
 import { createJobSchema } from "@/schema/createJobSchema";
+
+import { put } from "vercel/blob";
+import { toSlug } from "@utils/toSlug";
 
 export async function createJobPosting(formData: FormData) {
   //convert the formData into an object with Object.fromEntries([])
@@ -8,4 +11,8 @@ export async function createJobPosting(formData: FormData) {
 
   const { title, location, type, companyLogo, companyName, locationType } =
     createJobSchema.parse(values);
+
+  const slug = `${toSlug(title)}-${nanoid(10)}`;
+  // upload to vercel blob storage
+  let companyLogoUrl: string | undefined = undefined;
 }
