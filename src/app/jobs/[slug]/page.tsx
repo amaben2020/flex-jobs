820e280e175a641e4a8a9fe93d7dd4d1e6cf9204
algomachine007 <benneth@izesan.com>
2024-04-16 10:30:06 +0100

    building the job page and application button

--- src/app/jobs/[slug]/page.tsx
+++ src/app/jobs/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import prisma from "@db/db";
 import { Metadata } from "next";
 import Image from "next/image";
@@ -39,22 +41,43 @@ export async function generateMetadata({
 
 const JobPage = async ({ params: { slug } }: IJob) => {
   const job = await getJob(slug);
-
+  const applicationLink = job.applicationEmail
+    ? `mailto:${job.applicationEmail}`
+    : job.applicationUrl;
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 md:flex-row md:items-start">
-      <section className="w-full grow space-y-5">
-        <div className="items-center gap-3">
+      <section className="flex w-full grow justify-between space-y-5">
+        <div className="  flex-grow  items-center gap-3 space-y-4">
           {job.companyLogo && (
             <Image
               src={job.companyLogo}
               alt="Company logo"
               width={100}
               height={100}
-              className="rounded-xl"
+              className=" rounded-xl"
             />
           )}
-          <p className="text-black">{JSON.stringify(job)}</p>
+
+          <h3>Company {job.companyName}</h3>
+          <p>Location: {job.location}</p>
+          <p>Salary: {job.salary}</p>
+          <p>Responsibilities & Duties: {job.description}</p>
+          <p>
+            {job.approved && <Badge className="bg-green-600">Approved </Badge>}
+          </p>
         </div>
+
+        <aside>
+          <Button asChild>
+            <a
+              href={applicationLink!!}
+              target="_blank"
+              className="w-40 md:w-fit"
+            >
+              Apply Now
+            </a>
+          </Button>
+        </aside>
       </section>
     </main>
   );
